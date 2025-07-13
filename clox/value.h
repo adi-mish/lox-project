2b6cfe2a53fa1b139710af842a9287561d112169
adi-mish <adimishra545@gmail.com>
2025-07-13 14:53:04 -0400

    Refactor code for consistency and readability; update function signatures and formatting across multiple files. completed till chapter 13

--- clox/value.h
+++ clox/value.h
@@ -6,13 +6,13 @@
 typedef double Value;
 
 typedef struct {
-    int capacity;
-    int count;
-    Value* values;
+  int capacity;
+  int count;
+  Value *values;
 } ValueArray;
 
-void initValueArray(ValueArray* array);
-void writeValueArray(ValueArray* array, Value value);
-void freeValueArray(ValueArray* array);
+void initValueArray(ValueArray *array);
+void writeValueArray(ValueArray *array, Value value);
+void freeValueArray(ValueArray *array);
 
 #endif
