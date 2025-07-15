d1e13275a417643b8bf2395bb1b03eafcaa0e143
adi-mish <adimishra545@gmail.com>
2025-07-14 21:41:23 -0400

    Add support for new literals and comparison operations in VM and compiler
    - Introduced OP_NIL, OP_TRUE, OP_FALSE, OP_EQUAL, OP_GREATER, OP_LESS, and OP_NOT opcodes.
    - Updated binary and unary operation handling in the compiler.
    - Enhanced the disassembly process to include new opcodes.
    - Implemented value equality checks and improved print functionality for different value types. completed chapter 18

--- clox/vm.c
+++ clox/vm.c
@@ -60,6 +60,27 @@ static InterpretResult run() {
       push(constant);
       break;
     }
+    case OP_NIL:
+      push(NIL_VAL);
+      break;
+    case OP_TRUE:
+      push(BOOL_VAL(true));
+      break;
+    case OP_FALSE:
+      push(BOOL_VAL(false));
+      break;
+    case OP_EQUAL: {
+      Value b = pop();
+      Value a = pop();
+      push(BOOL_VAL(valuesEqual(a, b)));
+      break;
+    }
+    case OP_GREATER:
+      BINARY_OP(BOOL_VAL, >);
+      break;
+    case OP_LESS:
+      BINARY_OP(BOOL_VAL, <);
+      break;
     case OP_ADD:
       BINARY_OP(NUMBER_VAL, +);
       break;
@@ -71,6 +92,9 @@ static InterpretResult run() {
       break;
     case OP_DIVIDE:
       BINARY_OP(NUMBER_VAL, /);
+      break;
+    case OP_NOT:
+      push(BOOL_VAL(isFalsey(pop())));
       break;
     case OP_NEGATE:
       if (!IS_NUMBER(peek(0))) {
@@ -121,3 +145,7 @@ Value pop() {
 }
 
 static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
+
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
