ed0f0366fab8237cba4bd44f3a05d821fa2ac325
adi-mish <adimishra545@gmail.com>
2026-05-10 17:43:08 -0400

    Use scoped cpplox bytecode enums

--- cpplox/src/cpplox/bytecode/chunk.h
+++ cpplox/src/cpplox/bytecode/chunk.h
@@ -10,12 +10,17 @@ namespace cpplox {
 
 struct Entry;
 
-typedef enum {
-  CACHE_EMPTY,
-  CACHE_GLOBAL,
-  CACHE_FIELD,
-  CACHE_METHOD,
-} InlineCacheKind;
+enum class InlineCacheKind : uint8_t {
+  Empty,
+  Global,
+  Field,
+  Method,
+};
+
+inline constexpr InlineCacheKind CACHE_EMPTY = InlineCacheKind::Empty;
+inline constexpr InlineCacheKind CACHE_GLOBAL = InlineCacheKind::Global;
+inline constexpr InlineCacheKind CACHE_FIELD = InlineCacheKind::Field;
+inline constexpr InlineCacheKind CACHE_METHOD = InlineCacheKind::Method;
 
 typedef struct {
   InlineCacheKind kind;
@@ -30,70 +35,137 @@ typedef struct {
   Value value;
 } InlineCache;
 
-typedef enum {
-  OP_CONSTANT,
-  OP_CONSTANT_0,
-  OP_CONSTANT_1,
-  OP_CONSTANT_2,
-  OP_CONSTANT_3,
-  OP_CONSTANT_4,
-  OP_CONSTANT_5,
-  OP_CONSTANT_6,
-  OP_CONSTANT_7,
-  OP_NIL,
-  OP_TRUE,
-  OP_FALSE,
-  OP_POP,
-  OP_GET_LOCAL,
-  OP_GET_LOCAL_0,
-  OP_GET_LOCAL_1,
-  OP_GET_LOCAL_2,
-  OP_GET_LOCAL_3,
-  OP_GET_LOCAL_4,
-  OP_GET_LOCAL_5,
-  OP_GET_LOCAL_6,
-  OP_GET_LOCAL_7,
-  OP_SET_LOCAL,
-  OP_SET_LOCAL_0,
-  OP_SET_LOCAL_1,
-  OP_SET_LOCAL_2,
-  OP_SET_LOCAL_3,
-  OP_SET_LOCAL_4,
-  OP_SET_LOCAL_5,
-  OP_SET_LOCAL_6,
-  OP_SET_LOCAL_7,
-  OP_GET_GLOBAL,
-  OP_DEFINE_GLOBAL,
-  OP_SET_GLOBAL,
-  OP_GET_UPVALUE,
-  OP_SET_UPVALUE,
-  OP_GET_PROPERTY,
-  OP_SET_PROPERTY,
-  OP_GET_SUPER,
-  OP_EQUAL,
-  OP_GREATER,
-  OP_LESS,
-  OP_ADD,
-  OP_SUBTRACT,
-  OP_MULTIPLY,
-  OP_DIVIDE,
-  OP_NOT,
-  OP_NEGATE,
-  OP_PRINT,
-  OP_JUMP,
-  OP_JUMP_IF_FALSE,
-  OP_LOOP,
-  OP_CALL,
-  OP_INVOKE,
-  OP_SUPER_INVOKE,
-  OP_CLOSURE,
-  OP_CLOSE_UPVALUE,
-  OP_RETURN,
-  OP_CLASS,
-  OP_INHERIT,
-  OP_METHOD,
-  OP_COUNT
-} OpCode;
+enum class Opcode : uint8_t {
+  Constant,
+  Constant0,
+  Constant1,
+  Constant2,
+  Constant3,
+  Constant4,
+  Constant5,
+  Constant6,
+  Constant7,
+  Nil,
+  True,
+  False,
+  Pop,
+  GetLocal,
+  GetLocal0,
+  GetLocal1,
+  GetLocal2,
+  GetLocal3,
+  GetLocal4,
+  GetLocal5,
+  GetLocal6,
+  GetLocal7,
+  SetLocal,
+  SetLocal0,
+  SetLocal1,
+  SetLocal2,
+  SetLocal3,
+  SetLocal4,
+  SetLocal5,
+  SetLocal6,
+  SetLocal7,
+  GetGlobal,
+  DefineGlobal,
+  SetGlobal,
+  GetUpvalue,
+  SetUpvalue,
+  GetProperty,
+  SetProperty,
+  GetSuper,
+  Equal,
+  Greater,
+  Less,
+  Add,
+  Subtract,
+  Multiply,
+  Divide,
+  Not,
+  Negate,
+  Print,
+  Jump,
+  JumpIfFalse,
+  Loop,
+  Call,
+  Invoke,
+  SuperInvoke,
+  Closure,
+  CloseUpvalue,
+  Return,
+  Class,
+  Inherit,
+  Method,
+  Count
+};
+
+inline constexpr uint8_t opcodeByte(Opcode opcode) {
+  return static_cast<uint8_t>(opcode);
+}
+
+inline constexpr uint8_t OP_CONSTANT = opcodeByte(Opcode::Constant);
+inline constexpr uint8_t OP_CONSTANT_0 = opcodeByte(Opcode::Constant0);
+inline constexpr uint8_t OP_CONSTANT_1 = opcodeByte(Opcode::Constant1);
+inline constexpr uint8_t OP_CONSTANT_2 = opcodeByte(Opcode::Constant2);
+inline constexpr uint8_t OP_CONSTANT_3 = opcodeByte(Opcode::Constant3);
+inline constexpr uint8_t OP_CONSTANT_4 = opcodeByte(Opcode::Constant4);
+inline constexpr uint8_t OP_CONSTANT_5 = opcodeByte(Opcode::Constant5);
+inline constexpr uint8_t OP_CONSTANT_6 = opcodeByte(Opcode::Constant6);
+inline constexpr uint8_t OP_CONSTANT_7 = opcodeByte(Opcode::Constant7);
+inline constexpr uint8_t OP_NIL = opcodeByte(Opcode::Nil);
+inline constexpr uint8_t OP_TRUE = opcodeByte(Opcode::True);
+inline constexpr uint8_t OP_FALSE = opcodeByte(Opcode::False);
+inline constexpr uint8_t OP_POP = opcodeByte(Opcode::Pop);
+inline constexpr uint8_t OP_GET_LOCAL = opcodeByte(Opcode::GetLocal);
+inline constexpr uint8_t OP_GET_LOCAL_0 = opcodeByte(Opcode::GetLocal0);
+inline constexpr uint8_t OP_GET_LOCAL_1 = opcodeByte(Opcode::GetLocal1);
+inline constexpr uint8_t OP_GET_LOCAL_2 = opcodeByte(Opcode::GetLocal2);
+inline constexpr uint8_t OP_GET_LOCAL_3 = opcodeByte(Opcode::GetLocal3);
+inline constexpr uint8_t OP_GET_LOCAL_4 = opcodeByte(Opcode::GetLocal4);
+inline constexpr uint8_t OP_GET_LOCAL_5 = opcodeByte(Opcode::GetLocal5);
+inline constexpr uint8_t OP_GET_LOCAL_6 = opcodeByte(Opcode::GetLocal6);
+inline constexpr uint8_t OP_GET_LOCAL_7 = opcodeByte(Opcode::GetLocal7);
+inline constexpr uint8_t OP_SET_LOCAL = opcodeByte(Opcode::SetLocal);
+inline constexpr uint8_t OP_SET_LOCAL_0 = opcodeByte(Opcode::SetLocal0);
+inline constexpr uint8_t OP_SET_LOCAL_1 = opcodeByte(Opcode::SetLocal1);
+inline constexpr uint8_t OP_SET_LOCAL_2 = opcodeByte(Opcode::SetLocal2);
+inline constexpr uint8_t OP_SET_LOCAL_3 = opcodeByte(Opcode::SetLocal3);
+inline constexpr uint8_t OP_SET_LOCAL_4 = opcodeByte(Opcode::SetLocal4);
+inline constexpr uint8_t OP_SET_LOCAL_5 = opcodeByte(Opcode::SetLocal5);
+inline constexpr uint8_t OP_SET_LOCAL_6 = opcodeByte(Opcode::SetLocal6);
+inline constexpr uint8_t OP_SET_LOCAL_7 = opcodeByte(Opcode::SetLocal7);
+inline constexpr uint8_t OP_GET_GLOBAL = opcodeByte(Opcode::GetGlobal);
+inline constexpr uint8_t OP_DEFINE_GLOBAL = opcodeByte(Opcode::DefineGlobal);
+inline constexpr uint8_t OP_SET_GLOBAL = opcodeByte(Opcode::SetGlobal);
+inline constexpr uint8_t OP_GET_UPVALUE = opcodeByte(Opcode::GetUpvalue);
+inline constexpr uint8_t OP_SET_UPVALUE = opcodeByte(Opcode::SetUpvalue);
+inline constexpr uint8_t OP_GET_PROPERTY = opcodeByte(Opcode::GetProperty);
+inline constexpr uint8_t OP_SET_PROPERTY = opcodeByte(Opcode::SetProperty);
+inline constexpr uint8_t OP_GET_SUPER = opcodeByte(Opcode::GetSuper);
+inline constexpr uint8_t OP_EQUAL = opcodeByte(Opcode::Equal);
+inline constexpr uint8_t OP_GREATER = opcodeByte(Opcode::Greater);
+inline constexpr uint8_t OP_LESS = opcodeByte(Opcode::Less);
+inline constexpr uint8_t OP_ADD = opcodeByte(Opcode::Add);
+inline constexpr uint8_t OP_SUBTRACT = opcodeByte(Opcode::Subtract);
+inline constexpr uint8_t OP_MULTIPLY = opcodeByte(Opcode::Multiply);
+inline constexpr uint8_t OP_DIVIDE = opcodeByte(Opcode::Divide);
+inline constexpr uint8_t OP_NOT = opcodeByte(Opcode::Not);
+inline constexpr uint8_t OP_NEGATE = opcodeByte(Opcode::Negate);
+inline constexpr uint8_t OP_PRINT = opcodeByte(Opcode::Print);
+inline constexpr uint8_t OP_JUMP = opcodeByte(Opcode::Jump);
+inline constexpr uint8_t OP_JUMP_IF_FALSE = opcodeByte(Opcode::JumpIfFalse);
+inline constexpr uint8_t OP_LOOP = opcodeByte(Opcode::Loop);
+inline constexpr uint8_t OP_CALL = opcodeByte(Opcode::Call);
+inline constexpr uint8_t OP_INVOKE = opcodeByte(Opcode::Invoke);
+inline constexpr uint8_t OP_SUPER_INVOKE = opcodeByte(Opcode::SuperInvoke);
+inline constexpr uint8_t OP_CLOSURE = opcodeByte(Opcode::Closure);
+inline constexpr uint8_t OP_CLOSE_UPVALUE = opcodeByte(Opcode::CloseUpvalue);
+inline constexpr uint8_t OP_RETURN = opcodeByte(Opcode::Return);
+inline constexpr uint8_t OP_CLASS = opcodeByte(Opcode::Class);
+inline constexpr uint8_t OP_INHERIT = opcodeByte(Opcode::Inherit);
+inline constexpr uint8_t OP_METHOD = opcodeByte(Opcode::Method);
+inline constexpr int OP_COUNT = static_cast<int>(Opcode::Count);
 
 class Chunk {
 public:
